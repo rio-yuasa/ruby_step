2640ab18bb875863045231afd2f1ebbd9bd54371
rio-yuasa <rio.yuasa@smarthr.co.jp>
2022-09-16 18:34:43 +0900

    2022年9月16日の成果物

---
+++ 20220916/02/special_arg1.rb
@@ -0,0 +1,8 @@
+def calc_tax(price, tax_rate)
+    tax = (price * tax_rate).to_i
+    tax_included = price + tax
+    { tax: tax, tax_included: tax_included }
+end
+
+p calc_tax(100, 0.08)
+p calc_tax(100)
